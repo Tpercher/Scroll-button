5f862acd23f3424dfd26231ec9cfb4d06276e57c
Théo Percheron <77283423+Tpercher@users.noreply.github.com>
2021-02-23 16:52:05 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title> Site professionnel de Theo Percheron </title>
+		<meta name="description" content="description à inscrire ici" />
+		<meta name="google-site-verification" content="3I7gOh7jw9or_G40-4XOEO6GEYsEV0ZG-ZbfzA84PtM" />
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="style.css">
+		<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:400,300" />
+		<link rel="canonical" href="https://theopercheron.com/" />
+	</head>
+	<body>
+
+	  <button onclick="topFunction()" id="btnScroll" title="Go to top"><i class="fa fa-arrow-up fa-6" aria-hidden="true"></i></button>
+
+<footer>
+		<a href="index.php" title="mon site">Theo Percheron</a>
+		<span> | &copy; 2017 | </span>
+		<a href="#" title="mentions légales"> Tout droit réservé </a>
+	</footer>
+	</body>
+	<script src="js/script.js"></script><!-- Si je le place en haut cela fait bugger les modal -->
+</html>
